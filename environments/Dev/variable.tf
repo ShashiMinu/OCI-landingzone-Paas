f1d6f1ee793e354974e5844ca7c98c2f647ae274
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-27 08:44:47 +0530

    Update variable.tf

--- environments/Dev/variable.tf
+++ environments/Dev/variable.tf
@@ -1,7 +1,6 @@
 variable "compartment_name" { type = string}
 variable "bucket_name" { type = string }
 variable "tenancy_ocid" { type = string }
-variable "tenancy_ocid" { type = string }
 variable "user_ocid" {type = string}
 variable "fingerprint" {type = string}
 variable "private_key" {
